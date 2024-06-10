3f979f50a2d20c01c1caf45ce9687578fd960bcf
nasko25 <apashov0@gmail.com>
2024-06-10 12:00:09 +0300

    customization and shipping

--- frames/app/frames/buy/route.tsx
+++ frames/app/frames/buy/route.tsx
@@ -3,20 +3,23 @@ import { Button } from "frames.js/next";
 import { frames } from "../frames";
 
 const handleRequest = frames(async (ctx) => {
-  if (ctx.message?.transactionId) {
+  if (ctx.message?.transactionId || ctx.url.searchParams.has("example")) {
     return {
       image: (
-        <div tw="bg-purple-800 text-white w-full h-full justify-center items-center flex">
-          Transaction submitted! {ctx.message.transactionId}
+        <div tw="bg-purple-800 text-white w-full h-full justify-center items-center flex text-center">
+          Congratulations! Your purchase was successful. You can expect shipment in around 4-5 days.
+          This is the ID of your transaction: 1234
+          {/* {ctx.message.transactionId} */}
         </div>
       ),
-      imageOptions: {
-        aspectRatio: "1:1",
-      },
+      // imageOptions: {
+      //   aspectRatio: "1:1",
+      // },
       buttons: [
         <Button
           action="link"
-          target={`https://www.onceupon.gg/tx/${ctx.message.transactionId}`}
+          target={`https://google.com`}
+          // target={`https://www.onceupon.gg/tx/${ctx.message.transactionId}`}
         >
           View on block explorer
         </Button>,
@@ -26,19 +29,31 @@ const handleRequest = frames(async (ctx) => {
 
   return {
     image: (
-      <div tw="bg-800 text-white w-full h-full justify-center items-center flex relative">
-       <img src="https://cdn.iconscout.com/icon/free/png-256/free-t-shirt-2772271-2302029.png"></img>
-       <div tw="text-black absolute m-auto bottom-40"> Buy this T-Shirt? </div>
+      <div tw="bg-800 text-black w-full h-full justify-between items-center flex flex-col">
+        {/* {ctx.message?.inputText ? `, Typed: ${ctx.message?.inputText}` : "no text yet"} */}
+        { ctx.url.searchParams.has("color") && ctx.url.searchParams.get("color") === "black"
+          ? <img tw="h-200" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNMjLY3ExAv8O2swxPg7_vgt-s6D8t3chYDg&s"></img>
+          : <img tw="h-200" src="https://cdn.iconscout.com/icon/free/png-256/free-t-shirt-2772271-2302029.png"></img> }
+          Size: {ctx.url.searchParams.get("size")}
+        { ctx.url.searchParams.has("street") && ctx.message?.inputText ? 
+              <div tw="justify-between items-center flex flex-col"><p tw="mx-8 text-center">This is the address you have typed: {ctx.url.searchParams.get("street")}; {ctx.message?.inputText}</p>
+              <p tw="mx-8 text-center">If it is correct, please proceed to the checkout.</p></div>
+        : <div tw="justify-between items-center flex flex-col">
+              <p tw="mx-8 text-center">If you'd like to order this T-Shirt, please input your shipment details below.</p>
+              <p tw="mx-8 text-center">In this screen, please type in {ctx.message?.inputText ? "your city and country names, separated by a comma" : "your street name" }.</p>          
+          </div> }
       </div>
     ),
     imageOptions: {
       aspectRatio: "1:1",
     },
     buttons: [
-      <Button action="tx" target="/txdata" post_url="/">
-        Buy a unit
-      </Button>,
+      <Button action="post"> Cancel </Button>, 
+      ctx.url.searchParams.has("street") && ctx.message?.inputText ? <Button action="tx" target="/txdata" post_url="/">
+      Checkout 
+     </Button> : <Button action="post" target={{ pathname: "/buy", query: ctx.message?.inputText ? { ...Object.fromEntries(ctx.url.searchParams), street: ctx.message?.inputText } : Object.fromEntries(ctx.url.searchParams) }}>Proceed</Button>,
     ],
+    textInput: ctx.url.searchParams.has("street") ? undefined : ctx.message?.inputText ? "City and country" : "Street and number",
   };
 });
 
--- frames/app/frames/route.tsx
+++ frames/app/frames/route.tsx
@@ -4,31 +4,26 @@ import { frames } from "./frames";
 
 // TODO: fetch from shopify
 const handleRequest = frames(async (ctx) => {
+  // NOTE: keep in mind that one object is the "default" one (when no parameters are selected)
   return {
-    image: ctx.message ? (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        GM, {ctx.message.requesterUserData?.displayName}! Your FID is{" "}
-        {ctx.message.requesterFid}
-        {", "}
-        {ctx.message.requesterFid < 20_000
-          ? "you're OG!"
-          : "welcome to the Farcaster!"}
+    image: (
+      <div tw="text-black w-full h-full justify-center items-center flex relative">
+        <div tw="text-black w-140 h-140 justify-center items-center flex relative">
+          { ctx.url.searchParams.has("color") && ctx.url.searchParams.get("color") === "black"
+          ? <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNMjLY3ExAv8O2swxPg7_vgt-s6D8t3chYDg&s"></img>
+          : <img src="https://cdn.iconscout.com/icon/free/png-256/free-t-shirt-2772271-2302029.png"></img> }
+        </div>
+        <p tw="w-50 absolute right-0 top-0">Only 5 left in stock!</p>
       </div>
-    ) : (
-      ctx.url.searchParams.has("color") && ctx.url.searchParams.get("color") === "blk"
-      ? <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNMjLY3ExAv8O2swxPg7_vgt-s6D8t3chYDg&s"></img>
-      : <img src="https://cdn.iconscout.com/icon/free/png-256/free-t-shirt-2772271-2302029.png"></img>
     ),
+    // imageOptions: {
+    //   aspectRatio: "1:1"
+    // },
     buttons: [
-      <Button action="post" target={{ pathname: "/customize", query: { token: "asdasd-asdasd-dd" } }}>
+      <Button action="post" target={{ pathname: "/customize", query: Object.fromEntries(ctx.url.searchParams) }}>
         Customize 
       </Button>,
-      <Button action="post" target={{ pathname: "/buy" }}>
+      <Button action="post" target={{ pathname: "/buy", query: Object.fromEntries(ctx.url.searchParams) }}>
         Buy 
       </Button>,
     ]
