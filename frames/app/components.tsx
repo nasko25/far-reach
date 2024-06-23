cf3e1b3272af77b5abf0958cbcefbab6fe34759b
nasko25 <apashov0@gmail.com>
2024-06-24 01:20:06 +0300

    feat(frames): Design

--- frames/app/components.tsx
+++ frames/app/components.tsx
@@ -1,9 +1,9 @@
-export const NavBar = (props: { rank?: number}) => {
+export const NavBar = (props: { rank?: number, stats?: boolean}) => {
     // assumes parent is display: flex and has 100% width and height
     return (
         <nav tw="text-white border-gray-200 dark:bg-gray-900 border-b border-[#e5e7eb] h-40">
           <p tw="p-0 m-4 ml-20 my-auto"> Far Reach </p>
-          <p tw="m-4 mr-20 ml-auto my-auto p-0 max-w-lg"> { props.rank ? "Current Rank: {props.rank}" + "You are not yet a part of far reach. Please apply below." : "Web3 Affiliate Platform"} </p>
+          <p tw="m-4 mr-20 ml-auto my-auto p-0 max-w-lg"> { props.stats ? props.rank ? `Current Rank: ${props.rank}` : "You are not yet a part of far reach. Please apply below." : "Web3 Affiliate Platform"} </p>
         </nav>
       );
   }
@@ -21,44 +21,50 @@ export const NavBar = (props: { rank?: number}) => {
       );
   }
   
+  const randomBetween = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
   const FrameData = () => {
     return (
-        <div tw="flex flex-col m-0">
+        <div tw="flex flex-col m-0 mt-5">
             {
                 // I could not get the globals.css file to work...
             }
             <p tw="m-0" className="frame-data"> Published on: 23 June 2024 @ 12:23 </p>
             <div tw="flex flex-row">
                 <div tw="flex flex-col">
-                    <p tw="m-0" style={{"marginLeft": "20px", color: "red"}}> Total likes: 20 </p>
-                    <p tw="m-0" style={{"marginLeft": "20px"}}> Total recasts: 12 </p>
-                    <p tw="m-0" style={{"marginLeft": "20px"}}> Total comments: 42 </p>
+                    <p tw="m-0" style={{"marginLeft": "20px", color: "red"}}> Total likes: {randomBetween(10, 20)} </p>
+                    <p tw="m-0" style={{"marginLeft": "20px"}}> Total recasts: {randomBetween(2, 15)} </p>
+                    <p tw="m-0" style={{"marginLeft": "20px"}}> Total comments: {randomBetween(10, 60)} </p>
                 </div>
-                <img tw="m-auto mr-40 h-30" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNMjLY3ExAv8O2swxPg7_vgt-s6D8t3chYDg&s"></img>
+                <img tw="m-auto mr-40 h-30" src={Math.random() > 0.5 ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNMjLY3ExAv8O2swxPg7_vgt-s6D8t3chYDg&s" :"https://cdn.iconscout.com/icon/free/png-256/free-t-shirt-2772271-2302029.png" }></img>
             </div>
 
         </div>
     );
   }
 
-  export const Stats = (props: { displayName: string | undefined, totalRewards: number | undefined, rank: string, profileImage: string | undefined }) => {
+  export const Stats = (props: { displayName: string | undefined, totalRewards: number | undefined, profileImage: string | undefined }) => {
     // assumes parent is display: flex and has 100% width and height
     return (
-        <div tw="text-white border-gray-200 dark:bg-gray-900 mb-auto mt-20 ml-40 flex flex-col">
+        <div tw="text-white border-gray-200 dark:bg-gray-900 mb-auto mt-5 ml-40 flex flex-col">
           {/* TODO: display pfp, total far reacher posts & impressions, total money made, rank */}
           {/* if user is not far reacher yet ass a custom message saying no data to show */}
           {/* TODO: also need to handle null images (maybe some placeholder static pfp) */}
-          <div tw="flex flex-row">
+          <div tw="flex flex-row m-0">
             <div  tw="w-40 h-40 overflow-hidden flex m-0">
                 <img src={props.profileImage} alt="User Profile image"/>
             </div>
             <p tw="ml-20 max-w-60"> {props.displayName} </p>
-            {/* <p tw="ml-auto mr-10">  </p> */}
-            <p tw="m-0 ml-auto mr-40 w-70 text-center h-40"> Total rewards so far: ${props.totalRewards}</p>
+            <p tw="m-0 ml-auto mr-40 w-70 text-center h-40"> Total rewards so far: ${props.totalRewards ?? 0}</p>
           </div>
-          <div tw="flex border-b border-gray-700 mr-40 text-gray-400 mb-8"> </div>
-          <p> Your Top 3 frames (+ engagement) </p>
-          <FrameData/>
+          <div tw="flex border-b border-gray-700 mr-40 text-gray-400 mb-4 mt-0"> </div>
+          <p tw="mt-0 mr-40 justify-center"> Your Top 3 frames </p>
+          { props.totalRewards ? <FrameData/> : undefined }
+          { props.totalRewards ? <FrameData/> : undefined }
+          { props.totalRewards ? <FrameData/> : undefined }
+          { props.totalRewards ? undefined : <p tw="mr-40 justify-center">You don't have any frames yet</p>}
         </div>
       );
   }
--- frames/app/frames/stats/route.tsx
+++ frames/app/frames/stats/route.tsx
@@ -6,19 +6,20 @@ import { getUserDataForFid } from "frames.js";
 const handleRequest = frames(async (ctx) => {
     const userData = await getUserDataForFid({fid: ctx.message?.requesterFid!});
     console.log("user data from stats: " + JSON.stringify(userData));
+    const rank = 4;
     return {
       image: (
         <div tw="bg-[#17101f] text-black flex flex-col w-full h-full">
-            <NavBar rank={ 4 } />
-            <Stats profileImage={userData?.profileImage} displayName={userData?.displayName} totalRewards={400} rank="2"/>
+            <NavBar stats={ true } rank={ rank } />
+            <Stats profileImage={ userData?.profileImage } displayName={ userData?.displayName } totalRewards={ rank ? 400 : undefined} />
         </div>
       ),
-      imageOptions: {
+      imageOptions: rank ? {
         aspectRatio: "1:1",
-      },
-      buttons: [
+      } : undefined,
+      buttons: rank == undefined ? [
         <Button action="post" target={{pathname: "/apply"}}> Become a far reacher. </Button>
-      ]
+      ] : undefined
     };
   });
   
