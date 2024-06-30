ee415d75e5f3c5758b7557d59205322085ba2c46
nasko25 <apashov0@gmail.com>
2024-06-30 13:14:06 +0300

    feat(frames): user conversions

--- frames/app/api/analytics/[campaign_id]/route.ts
+++ frames/app/api/analytics/[campaign_id]/route.ts
@@ -1,4 +1,4 @@
-import { sql } from "@vercel/postgres";
+import { QueryResultRow, sql } from "@vercel/postgres";
 import Moralis from "moralis";
 
 Moralis.start({
@@ -15,10 +15,29 @@ export async function GET(
   const { rows: rowsTransactions } =
     await sql`SELECT * from TRANSACTION_EVENT where campaign_id=${params.campaign_id}`;
 
+  const uniqueRowsTransactions = rowsTransactions.reduce(
+    (acc: QueryResultRow[], obj) => {
+      if (acc.filter((r) => r.customer_fid === obj.customer_fid).length === 0) {
+        acc.push(obj);
+      }
+      return acc;
+    },
+    [] as QueryResultRow[]
+  );
+
   const networths = [];
   const customerFids = [];
   let customersAlsoBought = new Set<string>();
-  for (var row of rowsTransactions) {
+  const { rows: rowsUniqueCustomerTransactions } =
+    await sql`SELECT DISTINCT customer_fid from TRANSACTION_EVENT where campaign_id=${params.campaign_id}`;
+  const { rows: rowsUniqueCustomerInteractions } =
+    await sql`SELECT DISTINCT customer_fid from FRAME_INTERACTIONS where campaign_id=${params.campaign_id}`;
+
+  const userConversion =
+    rowsUniqueCustomerTransactions.length /
+    rowsUniqueCustomerInteractions.length;
+
+  for (var row of uniqueRowsTransactions) {
     customerFids.push(row.customer_fid);
     const { rows: rowsCustomersAlsoBought } =
       await sql`SELECT DISTINCT campaign_id from TRANSACTION_EVENT where customer_fid=${row.customer_fid} and campaign_id!=${params.campaign_id}`;
@@ -54,10 +73,10 @@ export async function GET(
   }
 
   return Response.json({
-    hello: "there",
     page: params.campaign_id,
     networths,
     customerFidsBelow10000: customerFids.filter((fid) => fid <= 10000).length,
     customersAlsoBought: Array.from(customersAlsoBought),
+    userConversion,
   });
 }
